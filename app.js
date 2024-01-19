4dabca860075fe49bc39712451fad85ce98ca87e
周政輝 <leo604leo604@gmail.com>
2024-01-19 21:35:56 +0800

    設定並完成安裝 SendGrid

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
 
 app.get('/', (req, res) => {
   res.send('Hello, Vercel!');
