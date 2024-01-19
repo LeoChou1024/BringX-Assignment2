8bf50ffb8d3e9ad25cddd501699210e7ad9e783c
周政輝 <leo604leo604@gmail.com>
2024-01-19 22:49:38 +0800

    完成專案，並將SendGrid API Key 存到環境變數

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('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');
-
+// @ts-ignore
+sgMail.setApiKey(process.env.SENDGRID);
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+console.log(process.env.SENDGRID);
 app.get('/', (req, res) => {
-  res.send('Hello, Vercel!');
+  res.send(process.env.SENDGRID);
 });
 
-const PORT = process.env.PORT || 3000;
+app.post('/mail', (req, res) => {
+  const postData = req.body;
+  const msg = {
+    to: postData.to,
+    from: 'leo604leo604@gmail.com',
+    subject: postData.subject,
+    text: postData.body,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      res.json('Email sent successfully');
+    })
+    .catch((error) => {
+      res.json('Error sending email:' + error);
+    });
+});
+
+const PORT = process.env.PORT || 8016;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
